b6c892d0c62dcc65cfb1e5db1d8ad25af3271755
Volodymyr Todosiuk <v.todosiuk@gmail.com>
2017-03-05 01:35:52 +0200

    Commit 7

--- src/main/java/com/lardi/dao/UserDao.java
+++ src/main/java/com/lardi/dao/UserDao.java
@@ -6,6 +6,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
