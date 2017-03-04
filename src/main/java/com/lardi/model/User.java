f2fd6e77f6863031386376acb1014805ddc3b79f
Volodymyr Todosiuk <v.todosiuk@gmail.com>
2017-03-04 13:00:44 +0200

    Commit 1

---
+++ src/main/java/com/lardi/model/User.java
@@ -0,0 +1,12 @@
+package com.lardi.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="user")
+public class User implements Serializable {
+
+}
