cf53dc3a31b3cdcc4a24e5ffddbe449f8c5193f4
Volodymyr Todosiuk <v.todosiuk@gmail.com>
2017-03-05 14:52:12 +0200

    Commit 9

--- src/test/java/com/lardi/daoTest/UserDaoImplTest.java
+++ src/test/java/com/lardi/daoTest/UserDaoImplTest.java
@@ -1,15 +1,13 @@
 package com.lardi.daoTest;
 
+import java.util.List;
+
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
-
 import com.lardi.app.PhoneBookTest;
 import com.lardi.dao.UserDaoImpl;
 import com.lardi.model.User;
@@ -18,18 +16,33 @@ import com.lardi.model.User;
 @SpringBootTest(classes = PhoneBookTest.class)
 public class UserDaoImplTest {
 
-	//@Autowired
-	//private TestEntityManager entityManager;
-
 	@Autowired
 	UserDaoImpl userDao;
 
 	@Test
-	public void save() {
+	public void saveTest() {
 		User user = new User();
 		user.setUserlogin("login");
 		user.setUserPassword("password");
 		user.setFullName("Fiomyfio");
 		userDao.create(user);
+		Assert.assertNotNull(user);
 	}
+
+	@Test
+	public void readTest() {
+		User user = new User();
+		user.setUserlogin("login");
+		user.setUserPassword("password");
+		user.setFullName("Fiomyfio");
+		userDao.create(user);
+
+		List<User> userList = userDao.read();
+
+		Assert.assertEquals("Fiomyfio", userList.get(0).getFullName());
+		Assert.assertEquals("login", userList.get(0).getUserlogin());
+		Assert.assertEquals("password", userList.get(0).getUserPassword());
+
+	}
+
 }
