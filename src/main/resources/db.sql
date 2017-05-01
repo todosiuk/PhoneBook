dcf86f1cdfb03bab92641e6dc91073f222230362
Volodymyr Todosiuk <v.todosiuk@gmail.com>
2017-05-01 17:13:49 +0300

    Commit 01.05.2017

--- src/main/resources/db.sql
+++ src/main/resources/db.sql
@@ -1,47 +1,52 @@
-CREATE DATABASE  IF NOT EXISTS `phonebook`;
-USE `phonebook`;
+CREATE DATABASE  IF NOT EXISTS phonebook;
+USE phonebook;
 
 --
 -- Table structure for table `role`
 --
 
-DROP TABLE IF EXISTS `role`;
-CREATE TABLE `role` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+DROP TABLE IF EXISTS role;
+CREATE TABLE role (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  name varchar(45) DEFAULT NULL,
+  PRIMARY KEY (id)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `role`
 --
 
-LOCK TABLES `role` WRITE;
-INSERT INTO `role` VALUES (1,'ROLE_USER');
+LOCK TABLES role WRITE;
+INSERT INTO role VALUES (1,'ROLE_USER');
 UNLOCK TABLES;
 
 --
 -- Table structure for table `user`
 --
+DROP TABLE IF EXISTS user;
+CREATE TABLE user (id int(11) not null auto_increment, name varchar(255), login
+varchar(255), password varchar(255),fullName varchar (255), primary key (id)
+)ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
-create table 'user' ('id' int(11) not null auto_increment, 'name' varchar(255), 'login'
-varchar(255), 'password' varchar(255), primary key (id))ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+LOCK TABLES user WRITE;
+INSERT INTO user VALUES (1,'user1', '12345', '12345','Tom Crus');
+UNLOCK TABLES;
 
 --
 -- Table structure for table `user_role`
 --
 
-DROP TABLE IF EXISTS `user_role`;
-CREATE TABLE `user_role` (
-  `user_id` int(11) NOT NULL,
-  `role_id` int(11) NOT NULL,
-  PRIMARY KEY (`user_id`,`role_id`),
-  KEY `fk_user_role_roleid_idx` (`role_id`),
-  CONSTRAINT `fk_user_role_roleid` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `fk_user_role_userid` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+DROP TABLE IF EXISTS user_role;
+CREATE TABLE user_role (
+  user_id int(11) NOT NULL,
+  role_id int(11) NOT NULL,
+  PRIMARY KEY (user_id,role_id),
+  KEY fk_user_role_roleid_idx (role_id),
+  CONSTRAINT fk_user_role_roleid FOREIGN KEY (role_id) REFERENCES role (id) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT fk_user_role_userid FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-create table 'contact' ('id' int(11) not null auto_increment, 'address' varchar(255),
-'email' varchar(255), 'home_phone' varchar(255), 'mobile_phone' varchar(255) not null,
-'name' varchar(255) not null, 'patronymic' varchar(255) not null, 'surname' varchar(255) not null, 
+create table contact (id int(11) not null auto_increment, address varchar(255),
+email varchar(255), home_phone varchar(255), mobile_phone varchar(255) not null,
+name varchar(255) not null, patronymic varchar(255) not null, surname varchar(255) not null, 
 user_iduser int(11), primary key (id));
--- src/test/java/com/lardi/app/PhoneBookTest.java
+++ src/test/java/com/lardi/app/PhoneBookTest.java
@@ -2,15 +2,9 @@ package com.lardi.app;
 
 import org.junit.Test;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan(value = "com.lardi")
-@EnableJpaRepositories(value = "com.lardi")
-@EntityScan(basePackages = { "com.lardi" }, basePackageClasses = { Jsr310JpaConverters.class })
+
 public class PhoneBookTest {
 
 	@Test
